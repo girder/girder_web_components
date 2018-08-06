d953b0b25210e68f394beef757c369e8307b8f2a
Zach Mullen <zach.mullen@kitware.com>
2018-08-06 15:21:18 -0400

    Remove a method that is trivial to call in axios

--- src/rest.js
+++ src/rest.js
@@ -12,17 +12,6 @@ export default class RestClient {
   }
 
   /**
-   * Register a callback function that will be called on error responses.
-   * @param {Function} fn The function to be called. The callback will receive
-   *     a single argument, an axios error object. If you wish to ignore the
-   *     error, return a value. If you wish to propagate the error, return
-   *     a rejected Promise with the original argument.
-   */
-  onErrorResponse(fn) {
-    this._axios.interceptors.response.use(undefined, fn);
-  }
-
-  /**
    * Use this to make Web API requests.
    * @returns An Object with the same API as axios.
    */
