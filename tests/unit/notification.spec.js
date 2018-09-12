520d0f6d6ec2fe843325664ae94611ba4a404dd5
Zach Mullen <zach.mullen@kitware.com>
2018-09-11 20:40:36 -0400

    Add unit tests for event stream mode

--- tests/unit/notification.spec.js
+++ tests/unit/notification.spec.js
@@ -20,6 +20,12 @@ function waitsFor(func, msg) {
   });
 }
 
+class MockEventSource {
+  close() {
+    return this;
+  }
+}
+
 describe('NotificationBus', () => {
   const rc = new RestClient();
   const mock = new MockAdapter(rc);
@@ -41,12 +47,12 @@ describe('NotificationBus', () => {
     bus.$on('message', message);
     bus.$on('message:A', messageA);
     bus.$on('message:A.B', messageAB);
-    expect(bus.connected).to.equal(false);
+    expect(bus.connected).to.be.false;
 
     mock.onGet(/notification/).reply(200, [notification]);
 
     bus.connect();
-    expect(bus.connected).to.equal(true);
+    expect(bus.connected).to.be.true;
 
     await waitsFor(
       () => message.calledOnce && messageA.calledOnce && messageAB.calledOnce,
@@ -54,6 +60,31 @@ describe('NotificationBus', () => {
     );
     expect(+bus.since).to.be.closeTo(+notification.updated, 10);
     bus.disconnect();
-    expect(bus.connected).to.equal(false);
+    expect(bus.connected).to.be.false;
+  });
+
+  it('event stream mode', () => {
+    const message = sinon.stub();
+    const error = sinon.stub();
+    const bus = new NotificationBus(rc, {
+      useEventSource: true,
+      EventSource: MockEventSource,
+    });
+    const notification = {
+      type: 'test',
+      updated: new Date(),
+    };
+    bus.$on('message', message);
+    bus.$on('error', error);
+
+    expect(bus._eventSource).to.be.undefined;
+    bus.connect();
+    expect(bus._eventSource).not.to.be.undefined;
+
+    bus._eventSource.onmessage({ data: JSON.stringify(notification) });
+    expect(message.calledOnce).to.be.true;
+
+    bus._eventSource.onerror();
+    expect(error.calledOnce).to.be.true;
   });
 });
