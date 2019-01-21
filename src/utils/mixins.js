2dce508d9061ca76251932e4e3161509c47bb296
Zach Mullen <zach.mullen@kitware.com>
2019-01-21 10:51:19 -0500

    Refs #62. Add mixin for checking resource access level

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,3 +1,5 @@
+import { AccessType } from '@/constants';
+
 /**
  * Any view component that needs to display human-readable data sizes should use this.
  */
@@ -19,4 +21,15 @@ const sizeFormatter = {
   },
 };
 
-export { sizeFormatter };
+/**
+ * This mixin exposes helper methods for components that need to check that the current user has
+ * a certain access level on a given resource.
+ */
+const accessLevelChecker = {
+  methods: {
+    hasWriteAccess: resource => resource._accessLevel >= AccessType.WRITE,
+    hasAdminAccess: resource => resource._accessLevel >= AccessType.ADMIN,
+  },
+};
+
+export { accessLevelChecker, sizeFormatter };
