17ae2939c0561279f8f616e672cf119ceedc803d
subdavis <brandon.davis@kitware.com>
2019-01-25 16:16:35 -0500

    Merge pull request #63 from girder/constants
    Add useful constants to the library

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import * as mixins from './mixins';
-import vuetifyConfig from './vuetifyConfig';
-import Upload from './upload';
-
-export {
-  mixins,
-  vuetifyConfig,
-  Upload,
-};
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/girder_web_components'
-    : '/',
-};
