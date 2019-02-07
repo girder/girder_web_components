59c7c3d74efd0b155df66d1d76e810717b45cfae
Brandon Davis <brandonadavis@outlook.com>
2019-02-07 16:15:06 -0500

    Circleci/deploy2 (#72)
    * Adds deployment to CircleCI
    
    * disable filtering
    
    * enable branch filtering
    
    * Add comment
    
    * combine first 2 jobs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   lintOnSave: false,
+  // publicPath only affects demo application deployed to GH pages.
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/girder_web_components'
+    : '/',
 };
