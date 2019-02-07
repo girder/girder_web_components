083a2a81b2da3edda40e3957201018fcc7d489ae
subdavis <brandon.davis@kitware.com>
2019-02-07 15:07:32 -0500

    Add comment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   lintOnSave: false,
+  // publicPath only affects demo application deployed to GH pages.
   publicPath: process.env.NODE_ENV === 'production'
     ? '/girder_web_components'
     : '/',
