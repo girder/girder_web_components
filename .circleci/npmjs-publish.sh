6c3cae83554ced5f5bf8d34293ec7a74e08d5d31
Brandon Davis <brandon.davis@kitware.com>
2019-10-08 10:29:08 -0400

    Add tagging

--- .circleci/npmjs-publish.sh
+++ .circleci/npmjs-publish.sh
@@ -13,6 +13,9 @@ export PUBLISHED_VERSION=$(npm show @girder/components version \
 
 if [ "$PACKAGE_VERSION" != "$PUBLISHED_VERSION" ]; then
   yarn publish --non-interactive
+  TAG_NAME="v${PACKAGE_VERSION}"
+  git tag "$TAG_NAME"
+  git push origin "refs/tags/$TAG_NAME"
 else
   echo "package version $PACKAGE_VERSION matches npmjs version $PUBLISHED_VERSION. Nothing to do."
 fi
