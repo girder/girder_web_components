2209eb43f7faad72e1db86a6609669a20ddefa04
Zach Mullen <zach.mullen@kitware.com>
2021-02-05 09:16:15 -0500

    New file upload process

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -28,21 +28,23 @@ export default class Upload extends UploadBase {
   }
 
   async start() {
-    const { value } = await this.s3FFClient.uploadFile(this.file, 'core.File.blob');
+    let fileModel;
+    let blobValue;
 
     try {
-      return (await this.$rest.post('files', {
+      fileModel = (await this.$rest.post('files', {
         name: this.file.name,
         content_type: this.file.type || 'application/octet-stream',
-        blob: value,
+        size: file.size,
         folder: this.parent.id,
       })).data;
     } catch (ex) {
       if (ex.response) {
         const data = ex.response.data;
-        // Are there any other categories of validation errors besides __all__ or name?
         if (data.name) {
           throw new Error(data.name[0]);
+        } else if (data.size) {
+          throw new Error(data.size[0]);
         } else if (data.__all__) {
           throw new Error(data.__all__[0]);
         } else {
@@ -53,5 +55,20 @@ export default class Upload extends UploadBase {
         throw new Error('Could not connect to the server.');
       }
     }
+
+    try {
+      blobValue = (await this.s3FFClient.uploadFile(this.file, 'core.File.blob')).value;
+    } catch (ex) {
+      console.error(ex);
+      throw new Error('An error occurred when uploading to storage. See console for details.');
+    }
+
+    try {
+      await this.$rest.patch(`files/${fileModel.id}`, { blob: blobValue });
+    } catch (ex) {
+      console.error(ex);
+      throw new Error('Could not finalize file upload. See console for details.');
+    }
+    return fileModel;
   }
 }
